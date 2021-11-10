eb6e6f911fa93ddc2923d2e6c96ed3f7a12bfcb0
Sam Okpara <samuelopra@gmail.com>
2021-11-10 13:25:19 -0500

    display coordinates and error handling

--- src/index.js
+++ src/index.js
@@ -14,15 +14,31 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: 40, long: null };
+    this.state = { lat: null, long: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) =>
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
   }
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+    if (this.state.lat && !this.state.errorMessage) {
+      return (
+        <div>
+          Latitude: {this.state.lat} | Longitude: {this.state.long}
+        </div>
+      );
+    }
 
-    return <div>Latitude: {this.state.lat}</div>;
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>{this.state.errorMessage}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
